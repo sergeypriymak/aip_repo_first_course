b49500efb4085e2efb1675265b8c7fda406ec788
yobbsosa <144769194+sergeypriymak@users.noreply.github.com>
2023-12-31 21:56:07 +0300

    Create main.cpp

---
+++ contest_02/01/main.cpp
@@ -0,0 +1,13 @@
+bool is_prime(int number) {
+    if (number <= 1) {
+        return false;
+    }
+
+    for (int i = 2; i * i <= number; i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
